67f10c063f5fe74f82e6a1c2fd2e9837b6075e1a
Pid24 <rofidnasifannafie@gmail.com>
2025-11-04 06:27:13 +0700

    user-input

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-name = "Rofid"
-age = 23
-gpa = 3.29
-is_student = False
+# name = input("What is ur name: ")
+# age = int(input("How old are u: "))
 
-gpa = int(gpa)
-print(type(age))
+# age = age + 1
+
+# print(f"Hello {name}")
+# print("Happy Birthday!")
+# print(f"You are {age} years old")
+
+# length = float(input("Enter the length: "))
+# width = float(input("Enter the width: "))
+# area = length * width
+
+# print(f"The area is: {area}cm")
+
+item = input("What item ?: ")
+price = float(input("price: "))
+quantity = int(input("How many would u like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"The total is: {total}")
