aa61860da9e998c18119002046eea3daab13cc56
Pid24 <rofidnasifannafie@gmail.com>
2025-11-03 14:59:44 +0700

    variables

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#This my first program in Python
-print('Hello World!')
-print('Aku Ganteng')
+#string
+first_name = "Pid"
+food = "kwetiau"
+email = "pid@gmail.com"
+
+#integer
+age = 23
+quantity = 3
+num_of_students = 30
+
+#float
+price = 10.99
+gpa = 3.29
+distance = 5.5
+
+#boolean
+is_student = true
