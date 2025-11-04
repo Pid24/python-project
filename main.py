db2aff08596981566d03d482d9bbde27488ada7a
Pid24 <rofidnasifannafie@gmail.com>
2025-11-04 13:12:53 +0700

    aritmatika-math

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing': ")
-adjective3 = input("Enter an adjective (description): ")
+import math
 
-print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, I saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"Side C = {c}")
