89a1f89aec090c5b0eaee5d49620742c93c11e4e
Pid24 <rofidnasifannafie@gmail.com>
2025-11-05 06:20:45 +0700

    if-statements

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import math
+# age = int(input("Enter ur age: "))
 
-a = float(input("Enter side A: "))
-b = float(input("Enter side B: "))
+# if age >= 100:
+#     print("U are too old to signed up!")
+# elif age >= 18:
+#     print("Ur are now signed up!")
+# elif age < 0:
+#     print("U haven't been born yet!")
+# else:
+#     print("U must be 18+ to signed up!")
 
-c = math.sqrt(pow(a, 2) + pow(b, 2))
+response = input("Would u like food? (Y/N): ")
 
-print(f"Side C = {c}")
+if response == "Y":
+    print("Have some food!")
+else:
+    print("No food for u")
